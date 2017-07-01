13b3fc5ca60f8f6290e19a82e94ad6ce90b43e3d
AV <aymfst@gmail.com>
2017-07-01 21:09:21 +0300

    Add rotation & scale

--- Assets/Scripts/ObjectPropertiesEditor.cs
+++ Assets/Scripts/ObjectPropertiesEditor.cs
@@ -9,6 +9,16 @@ public class ObjectPropertiesEditor : MonoBehaviour {
     public InputField posY;
     public InputField posZ;
 
+    [Header("Rotation")]
+    public InputField rotX;
+    public InputField rotY;
+    public InputField rotZ;
+
+    [Header("Scale")]
+    public InputField scaX;
+    public InputField scaY;
+    public InputField scaZ;
+
     private GameObject last;
     private GameObject current;
 
@@ -17,6 +27,14 @@ public class ObjectPropertiesEditor : MonoBehaviour {
         posX.onEndEdit.AddListener(delegate { ApplyValues(); });
         posY.onEndEdit.AddListener(delegate { ApplyValues(); });
         posZ.onEndEdit.AddListener(delegate { ApplyValues(); });
+
+        rotX.onEndEdit.AddListener(delegate { ApplyValues(); });
+        rotY.onEndEdit.AddListener(delegate { ApplyValues(); });
+        rotZ.onEndEdit.AddListener(delegate { ApplyValues(); });
+
+        scaX.onEndEdit.AddListener(delegate { ApplyValues(); });
+        scaY.onEndEdit.AddListener(delegate { ApplyValues(); });
+        scaZ.onEndEdit.AddListener(delegate { ApplyValues(); });
     }
 
     void Update()
@@ -37,6 +55,14 @@ public class ObjectPropertiesEditor : MonoBehaviour {
             posX.text = current.transform.position.x.ToString();
             posY.text = current.transform.position.y.ToString();
             posZ.text = current.transform.position.z.ToString();
+
+            rotX.text = current.transform.rotation.eulerAngles.x.ToString();
+            rotY.text = current.transform.rotation.eulerAngles.y.ToString();
+            rotZ.text = current.transform.rotation.eulerAngles.z.ToString();
+
+            scaX.text = current.transform.localScale.x.ToString();
+            scaY.text = current.transform.localScale.y.ToString();
+            scaZ.text = current.transform.localScale.z.ToString();
         }
     }
 
@@ -45,6 +71,8 @@ public class ObjectPropertiesEditor : MonoBehaviour {
         if (current != null)
         {
             ApplyPosition();
+            ApplyRotation();
+            ApplyScale();
         }
     }
 
@@ -71,6 +99,56 @@ public class ObjectPropertiesEditor : MonoBehaviour {
         catch (Exception e) { pZ = current.transform.position.z; }
 
         current.transform.position = new Vector3(pX, pY, pZ);
+    }
+
+    private void ApplyRotation()
+    {
+        float rX, rY, rZ;
+
+        try
+        {
+            rX = float.Parse(rotX.text);
+        }
+        catch (Exception e) { rX = current.transform.rotation.eulerAngles.x; }
+
+        try
+        {
+            rY = float.Parse(rotY.text);
+        }
+        catch (Exception e) { rY = current.transform.rotation.eulerAngles.y; }
+
+        try
+        {
+            rZ = float.Parse(rotZ.text);
+        }
+        catch (Exception e) { rZ = current.transform.rotation.eulerAngles.z; }
+
+        current.transform.rotation = Quaternion.Euler(rX, rY, rZ);
+    }
+
+    private void ApplyScale()
+    {
+        float sX, sY, sZ;
+
+        try
+        {
+            sX = float.Parse(scaX.text);
+        }
+        catch (Exception e) { sX = current.transform.localScale.x; }
+
+        try
+        {
+            sY = float.Parse(scaY.text);
+        }
+        catch (Exception e) { sY = current.transform.localScale.y; }
+
+        try
+        {
+            sZ = float.Parse(scaZ.text);
+        }
+        catch (Exception e) { sZ = current.transform.localScale.z; }
+
+        current.transform.localScale = new Vector3(sX, sY, sZ);
     }
 
 }
