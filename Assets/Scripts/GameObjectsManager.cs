67ab7821f10d599a4f3600fa094f498103b9958f
AV <aymfst@gmail.com>
2017-07-02 17:37:16 +0300

    Export level

--- Assets/Scripts/GameObjectsManager.cs
+++ Assets/Scripts/GameObjectsManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class GameObjectsManager : MonoBehaviour {
@@ -15,6 +17,64 @@ public class GameObjectsManager : MonoBehaviour {
             lastSelectedObject = GetSelectedObject();
             objectNameText.text = lastSelectedObject.name;
         }
+    }
+
+    public void SaveLevel()
+    {
+        List<GameObject> children = new List<GameObject>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            children.Add(transform.GetChild(i).gameObject);
+        }
+
+        string data = "";
+        foreach (GameObject go in children)
+        {
+            bool needToSaveChild = false;
+            switch (go.name)
+            {
+                case "Player": data += "player:"; needToSaveChild = true; break;
+                case "Finish": data += "finish:"; needToSaveChild = true; break;
+                case "Do not fall here!": data += "fall:"; break;
+            }
+
+            data += SaveGameObjectData(go);
+
+            if (needToSaveChild)
+            {
+                GameObject child = go.transform.GetChild(0).gameObject;
+                switch (go.name)
+                {
+                    case "Player(Top)": data += "playerTop:"; break;
+                    case "Finish(Top)": data += "finishTop:"; break;
+                }
+                data += SaveGameObjectData(child);
+            }
+        }
+
+        string path = Application.persistentDataPath + "/level_" + GenerateUniqueID() + ".txt";
+        System.IO.File.WriteAllText(path, data);
+        ShowExplorer(path);
+    }
+
+    private string SaveGameObjectData(GameObject go)
+    {
+        return go.transform.position.ToString() +
+               go.transform.rotation.ToString() +
+               go.transform.localScale.ToString() +
+               go.GetComponent<Renderer>().material.color.ToString() +
+               ";";
+    }
+
+    private string GenerateUniqueID()
+    {
+        return Guid.NewGuid().ToString("N");
+    }
+
+    private void ShowExplorer(string itemPath)
+    {
+        itemPath = itemPath.Replace(@"/", @"\");
+        System.Diagnostics.Process.Start("explorer.exe", "/select," + itemPath);
     }
 
     public void DeleteCurrent()
